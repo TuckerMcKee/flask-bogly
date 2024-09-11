8512176e87c2d4e7095e16674dd373f8cbeacd1e
Tucker McKee <tuckermckee@Tuckers-MacBook-Pro.local>
2024-09-11 12:16:33 -0500

    fixed tests

--- test.py
+++ test.py
@@ -1,22 +1,31 @@
 from unittest import TestCase
 from app import app
 from flask import session
+from sqlalchemy import text
 from models import db, connect_db, User
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///blogly_test'
 app.config['TESTING'] = True
 
-db.drop_all()
-db. create_all()
+# db.drop_all()
+# db.create_all()
 
 class FlaskTests(TestCase):
     
+    @classmethod
+    def setUpClass(cls):
+        with app.app_context():
+            db.drop_all()
+            db.create_all()
+
     def setUp(self):
         """create test user"""
+        self.app_context = app.app_context() 
+        self.app_context.push()
         User.query.delete()
-        user = {'first_name': 'James',
-                        'last_name':'Madison',
-                        'image_url':'https://science.nasa.gov/wp-content/uploads/2023/05/sun-cartoon-crop.png?w=4096&format=png&crop=1'}
+        db.session.execute(text('ALTER SEQUENCE users_id_seq RESTART WITH 1'))
+        user = User(first_name='James', last_name='Madison',
+                    image_url='https://science.nasa.gov/wp-content/uploads/2023/05/sun-cartoon-crop.png?w=4096&format=png&crop=1')
         db.session.add(user)
         db.session.commit()
 
@@ -50,5 +59,16 @@ class FlaskTests(TestCase):
             html = res.get_data(as_text=True)
 
             self.assertEqual(res.status_code, 200)
-            self.assertIn('test_first_name test_last_name',html)        
+            self.assertIn('test_first_name test_last_name',html)
+
+    def test_update_user_data(self):
+        """testing post route for editing user data"""
+        with app.test_client() as client:
+            edit_user = {'first_name': 'test_edit_first_name',
+                         'last_name': '',
+                         'image_url': ''}
+            res = client.post('/users/1/edit',data=edit_user,follow_redirects=True)
+            user = db.session.get(User, 1)
+            self.assertEqual(res.status_code, 200)
+            self.assertEqual(user.first_name,'test_edit_first_name')                 
    
